6005a7fc9d09027eca5af75c8c444d8719c78aec
dan_1 <dan_1@mail.ru>
2022-06-03 18:08:23 +0300

    second

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -6,6 +6,8 @@ const {
   DB_NAME,
 } = process.env;
 
+console.log(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`)
+
 module.exports = {
   url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
 };
